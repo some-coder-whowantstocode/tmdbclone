848ea8fdf3156cff7bbd238c300f6c7d49affb57
Rohit <rohit19003@gmail.com>
2023-05-07 19:22:22 +0530

    adding some features to searchpage

--- clone/src/Searchpage.js
+++ clone/src/Searchpage.js
@@ -1,10 +1,12 @@
-import React,{useState,useRef,useEffect,useCallback} from 'react'
+import React,{useState,useRef,useEffect,useCallback, useSyncExternalStore} from 'react'
 import axios from 'axios';
 import { Link, NavLink,useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import './searchpage.css';
 import image from'./inf.png';
 import Requests from './Requests';
+import './searchopt.css'
+import Profile from './profile.png'
 
 const Searchpage = () => {
 
@@ -17,6 +19,14 @@ const Searchpage = () => {
     const[pagenumber,setpagenumber]=useState(1);
     const[pn,setpn]=useState(1);
     const[newmovie,setnm]=useState();
+    const[sm,setsm]=useState([]);
+    const[st,setst]=useState([]);
+    const[sp,setsp]=useState([])
+    const[type,settype]= useState()
+    const[spl,setspl]=useState(0)
+    const[stl,setstl]=useState(0)
+    const[sml,setsml]=useState(0)
+    const[sl,setsl]=useState(0)
 
     
 
@@ -33,65 +43,81 @@ useEffect(()=>{
             const newurl = `https://api.themoviedb.org/3/search/multi?api_key=${Requests.apikey}&language=en-US&query=${elem}&page=${pn}}&include_adult=false`;
             const objdata = await axios.get(newurl);
             setnm(objdata.data.data ? objdata.data.data.results : objdata.data.results)
-   
         }
         loadmore();
     }
     catch(error){
-        ////console.log(error);
+        console.log(error.response?.objdata?.error);
     }
-    // console.log(pn)
-    
+   
+    console.log( "pn"+pn)
     
 },[pn])
+
+useEffect(()=>{
+    elem &&setspl(sp.length)
+},[sp])
+
+useEffect(()=>{
+    elem &&setstl(st.length)
+},[st])
+
+useEffect(()=>{
+    elem &&setsml(sm.length)
+},[sm])
+
+useEffect(()=>{
+    elem &&setsl(show.length)
+},[show])
 
 
 useEffect(()=>{
     if(newmovie != undefined && newmovie[0] == show[0]){
-        // console.log("stop")
     }
     else {
-        newmovie != undefined && newmovie && newmovie.map((res)=>(
-            setshow((prev)=>[...prev,res])
-            
-        ))
+
+
+        if (newmovie != undefined) {
+
+
+        newmovie.map((res)=>(
+                setshow((prev)=>[...prev,res])
+            ))
+
+
+            newmovie.map((res) => {
+              switch (res.media_type) {
+                case "movie": setsm((prev)=>[...prev,res])
+                  break;
+
+                case "tv": setst((prev)=>[...prev,res])
+                  break;
+
+                  case "person" : setsp((prev)=>[...prev,res])
+                  break;
+                  case "default" : console.log("nothing")
+              }
+            })
+          }
+       
     }
-  
-    
-    
 },[newmovie])
 
 
 
 const handleScroll =async()=>{
-
-   
-   
-
     try{
        
         if(window.innerHeight + document.documentElement.scrollTop + 30 >= document.documentElement.scrollHeight){
-        
             if(pn<limit){
-              
                 setpn((prev)=>prev + 1);
-                // console.log("fuck this is working")
-                
             }
-            else{
-                //console.log("fuck why the fuck is this not working")
-            }
-           
-            
         }
-  
             }
-            catch(error){
-                ////console.log(error);
+            catch(error){     
             }   
             finally{
                 setScrollTop(window.scrollY);
-                // console.log(window.scrollY)
             }
   }
 
@@ -100,9 +126,6 @@ const handleScroll =async()=>{
   const [scrollTop, setScrollTop] = useState(0);
 
   useEffect(() => {
-    // const handleScroll = event => {
-    //   setScrollTop(window.scrollY);
-    // };
 
     window.addEventListener('scroll', handleScroll);
 
@@ -114,13 +137,7 @@ const handleScroll =async()=>{
 
 
    
-//)
-
-useEffect(()=>{
-    // console.log(limit)
-},[limit])
-
-    
+//)    
       
 //create the number of pages so that unessesary runs won't happen(     
         useEffect(()=>{
@@ -135,8 +152,6 @@ useEffect(()=>{
 //shows search results when the search is done from home page(
     useEffect(()=>{
         setelem(location.state)
-        //console.log("elem")
-        
     },[location])
 
 //)
@@ -145,50 +160,42 @@ useEffect(()=>{
 //brings the result for the search on search bar
     useEffect(()=>{
         try{
-            //console.log(elem)
-
             const getdata  = async()=>{
                 if(elem != null){
                     const objdata = await axios.get(url);
                     
                     setmovies( objdata.data ? objdata.data : objdata.data)
                     setnm(objdata.data.data ? objdata.data.data.results : objdata.data.results)
-                    // ////console.log(show)
-                //    console.log(objdata!= [] && objdata)
                     setpn(objdata.data.page)
-                //    setpn(1)
                 setshow([])
-         
-                
+                setsm([])
+                setst([])
+                setsp([])
+                setspl(0)
+                setsml(0)
+                setstl(0)
+                setsl(0)
                 }
-              
-                
             }
             getdata()
         }
        catch(error){
-        //console.log(error);
+        console.log(error.response?.objdata?.response);
        }
-      
            setnm([])
         
     },[elem])
    
 //)
 
-useEffect(()=>{
-    // show != undefined && console.log(show)
-},[show])
+
 
     const inputelem = useRef("");
 
     const log = (e)=>{
         if(e.key=="Enter"){
             setelem(name)
-            //console.log(elem)
         }
-       
-        
     }
 
     const reg =(e)=>{
@@ -199,7 +206,7 @@ useEffect(()=>{
     const imag =(result)=>{
         switch(result.media_type){
           
-            case 'person': return  result.profile_path ? "https://image.tmdb.org/t/p/original"+result.profile_path : image
+            case 'person': return  result.profile_path ? "https://image.tmdb.org/t/p/original"+result.profile_path : Profile
       
            
             case 'movie': return  result.poster_path ? "https://image.tmdb.org/t/p/original"+result.poster_path : image
@@ -225,6 +232,153 @@ useEffect(()=>{
         }
     }
 
+const sho =(type)=>{
+    switch(type){
+        case "movie" : return (elem&&sm)&&sm.map((result)=>(
+            <div  className='resbox'>
+        
+                {
+                   result && result.media_type == "person" ?  <NavLink state={result.id} to="/profile">
+                    <div className="res">
+                    <img  className='image' src={imag(result) }></img>
+                    
+                    </div>
+        </NavLink>
+        : result.media_type == "tv" ? <NavLink state={result.id} to="/Tv">
+        <div className="res">
+        <img  className='image' src={imag(result) }></img>
+        
+        </div>
+        </NavLink>
+        :<NavLink state={result.id} to="/info">
+        <div className="res">
+        <img  className='image' src={imag(result) }></img>
+        
+        </div>
+        </NavLink>
+                }
+                   
+        <div className="sd">
+        <h5>{nam(result) }</h5>
+        <p className='chars'>{result.overview}</p>
+        </div>
+        
+        </div>  
+                ))
+
+        case "tv" : return (elem&&st)&&st.map((result)=>(
+                    <div  className='resbox'>
+                
+                        {
+                           result && result.media_type == "person" ?  <NavLink state={result.id} to="/profile">
+                            <div className="res">
+                            <img  className='image' src={imag(result) }></img>
+                            
+                            </div>
+                </NavLink>
+                : result.media_type == "tv" ? <NavLink state={result.id} to="/Tv">
+                <div className="res">
+                <img  className='image' src={imag(result) }></img>
+                
+                </div>
+                </NavLink>
+                :<NavLink state={result.id} to="/info">
+                <div className="res">
+                <img  className='image' src={imag(result) }></img>
+                
+                </div>
+                </NavLink>
+                        }
+                           
+                <div className="sd">
+                <h5>{nam(result) }</h5>
+                <p className='chars'>{result.overview}</p>
+                </div>
+                
+                </div>  
+                        ))
+
+
+        case "person" : return (elem&&sp)&&sp.map((result)=>(
+                            <div  className='resbox'>
+                        
+                                {
+                                   result && result.media_type == "person" ?  <NavLink state={result.id} to="/profile">
+                                    <div className="res">
+                                    <img  className='image' src={imag(result) }></img>
+                                    
+                                    </div>
+                        </NavLink>
+                        : result.media_type == "tv" ? <NavLink state={result.id} to="/Tv">
+                        <div className="res">
+                        <img  className='image' src={imag(result) }></img>
+                        
+                        </div>
+                        </NavLink>
+                        :<NavLink state={result.id} to="/info">
+                        <div className="res">
+                        <img  className='image' src={imag(result) }></img>
+                        
+                        </div>
+                        </NavLink>
+                                }
+                                   
+                        <div className="sd">
+                        <h5>{nam(result) }</h5>
+                        <p className='chars'>{result.overview}</p>
+                        </div>
+                        
+                        </div>  
+                                ))
+
+
+       default : return(elem&&show)&&show.map((result)=>(
+        <div  className='resbox'>
+    
+            {
+               result && result.media_type == "person" ?  <NavLink state={result.id} to="/profile">
+                <div className="res">
+                <img  className='image' src={imag(result) }></img>
+                
+                </div>
+    </NavLink>
+    : result.media_type == "tv" ? <NavLink state={result.id} to="/Tv">
+    <div className="res">
+    <img  className='image' src={imag(result) }></img>
+    
+    </div>
+    </NavLink>
+    :<NavLink state={result.id} to="/info">
+    <div className="res">
+    <img  className='image' src={imag(result) }></img>
+    
+    </div>
+    </NavLink>
+            }
+               
+    <div className="sd">
+    <h5>{nam(result) }</h5>
+    <p className='chars'>{result.overview}</p>
+    </div>
+    
+    </div>  
+            ))
+    }
+}
+
+const change =(type)=>{
+    // return sho(type)
+    settype(type)
+    switch(type){
+        case "movie" : 
+    }
+}
+
+const gototop=()=>{
+    window.scrollTo({top : 0,left : 0 ,behavior:'smooth'})
+}
+
+
   return (
     <>
       <Navbar/>
@@ -236,45 +390,31 @@ useEffect(()=>{
       </nav>
      
      
-    
+    <div className='searchbigbox'>
+<div className="serchresultbox">
 
+<div className="sebleft">
 <div className="serresult">
-{(elem&&show)&&show.map((result)=>(
-    <div key={result.id} className='resbox'>
-
-        {
-           result && result.media_type == "person" ?  <NavLink state={result.id} to="/profile">
-            <div className="res">
-            <img key={result.id} className='image' src={imag(result) }></img>
-            
-            </div>
-</NavLink>
-: result.media_type == "tv" ? <NavLink state={result.id} to="/Tv">
-<div className="res">
-<img key={result.id} className='image' src={imag(result) }></img>
+    {sho(type)}
 
 </div>
-</NavLink>
-:<NavLink state={result.id} to="/info">
-<div className="res">
-<img key={result.id} className='image' src={imag(result) }></img>
-
 </div>
-</NavLink>
-        }
-           
-<div className="sd">
-<h5>{nam(result) }</h5>
-<p className='chars'>{result.overview}</p>
+
+<div className="sebright">
+<div className='optionbox'>
+        <div className="obhead" >Results </div>
+      <div className="obmovie" onClick={()=>change("movie")}><span>Movie</span> <span className='obs'>{sml}</span></div>
+      <div className="obtvseries" onClick={()=>change("tv")}><span>Tv-Series</span> <span className='obs'>{stl}</span></div> 
+       <div className="obperson" onClick={()=>change("person")}><span>Person</span> <span className='obs'>{spl}</span></div>
+      <div className='oball' onClick={()=>change()}><span>All</span> <span className='obs'>{sl}</span></div>
+    </div>
 </div>
 
 </div>
 
-          
-            
-        ))}
 </div>
-     
+
+<button className='gototop' onClick={gototop}>↑</button>
        
     </>
   )
